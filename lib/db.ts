d9b1b39ddb67ef6c409acc107fec1cb1e8eca172
saldfuel <saldfuin63@gmail.com>
2025-05-25 19:14:43 +0530

    2commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,13 @@
 import Database from 'better-sqlite3';
 
+declare global {
+  // eslint-disable-next-line no-var
+  var db: Database.Database | undefined;
+}
+
 let db: Database.Database;
 
-if (!globalThis.db) {
+if (!global.db) {
   db = new Database('db.sqlite');
   db.pragma('journal_mode = WAL');
   db.exec(`
@@ -15,9 +20,9 @@ if (!globalThis.db) {
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     );
   `);
-  globalThis.db = db;
+  global.db = db;
 } else {
-  db = globalThis.db;
+  db = global.db;
 }
 
 export default db;
